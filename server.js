4f21f182d84cf04262f15312aa1487b973c6b3f8
antarid <tarand007@gmail.com>
2018-11-25 09:26:00 +0300

    build added 8 time

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.static('build'));
+app.use(express.static('build'));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(`${__dirname}/build/index.html`);
-// });
-
-app.get('/', (req, res) => {
-  res.send('hello word');
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/build/index.html`);
 });
+
+// app.get('/', (req, res) => {
+//   res.send('hello word');
+// });
 
 const port = process.env.PORT || 8080;
 
