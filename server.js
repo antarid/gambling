48d53853110a1f3d0b33476d6e8f142cc0394e53
antarid <tarand007@gmail.com>
2018-11-25 09:20:44 +0300

    hello world

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('build'));
+// app.use(express.static('build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/build/index.html`);
+// app.get('*', (req, res) => {
+//   res.sendFile(`${__dirname}/build/index.html`);
+// });
+
+app.get('/', (req, res) => {
+  res.send('hello word');
 });
 
-const port = process.env.PORT || 9090;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log('server is working on port: ' + port);
